a90884cc30b0bc5cb886876eeb32d44b15bc03ee
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-01 10:32:37 +0300

    "Added contact coach form and request received page, updated store with requests module"

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './module/coaches/index.js';
+import requestsModule from './module/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: requestsModule,
   },
   state() {
     return {
